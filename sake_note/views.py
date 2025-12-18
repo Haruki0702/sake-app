0109c331748084fa043d1f8feaf5e223715abcc3
Haruki0702 <haru702swkinkame@gmail.com>
2025-12-18 21:23:41 +0900

    Revert "Revert "add login""
    This reverts commit 4f4ea7024baafeb99488f4d6d9d017b5abe6d709.

--- sake_note/views.py
+++ sake_note/views.py
@@ -3,7 +3,8 @@ from .models import Sake
 from .forms import SakeForm
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
 
 
 class IndexView(ListView):
@@ -12,20 +13,38 @@ class IndexView(ListView):
     context_object_name="sake_list"
     ordering=["-created_at"]
 
-class SakeCreateView(CreateView):
+class SakeCreateView(LoginRequiredMixin, CreateView):
     model=Sake
     form_class=SakeForm
     template_name="sake_note/sake_form.html"
     success_url=reverse_lazy("index")
 
-class SakeUpdateView(UpdateView):
+    def form_valid(self, form):
+        form.instance.user=self.request.user
+        return super().form_valid(form)    
+
+class SakeUpdateView(LoginRequiredMixin, UpdateView):
     model=Sake
     form_class=SakeForm
     template_name="sake_note/sake_form.html"
     success_url=reverse_lazy("index")
 
-class SakeDeleteView(DeleteView):
+    def test_func(self):
+        obj=self.get_object()
+        return obj.user==self.request.user
+
+class SakeDeleteView(LoginRequiredMixin, DeleteView):
     model=Sake
     template_name="sake_note/sake_confirm_delete.html"
     success_url=reverse_lazy("index")
+
+    def test_func(self):
+        obj=self.get_object()
+        return obj.user==self.request.user
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy("login")
+
 

41254020b76d6375c403dee846a994824c0def89
Haruki0702 <haru702swkinkame@gmail.com>
2025-12-19 05:28:00 +0900

    modify function, scraping

--- sake_event_scraping/views.py
+++ sake_event_scraping/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .event_scraping import get_sake_events
+from .event_scraping import all_events
 from django.core.cache import cache
 
 def event_list(request):
     events=cache.get("sake_events")
     if not events:
         print("キャッシュなし。スクレイピング実行。")
-        events=get_sake_events()
+        events=all_events()
         cache.set("sake_events", events, 60*60)
     else:
         print("キャッシュから読み込み")

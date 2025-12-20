d08fa5925dcae86c9e291d47ee38af1588819c7f
Haruki0702 <haru702swkinkame@gmail.com>
2025-12-21 01:49:11 +0900

    modify, urls.py

--- sake_project/urls.py
+++ sake_project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from sake_ranking import views as api_views
+from sake_note import api_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
